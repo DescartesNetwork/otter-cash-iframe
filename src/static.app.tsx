b95f537db2703124f3e9c2cbbc4c57ee744024f7
tuphan-dn <tuphan@descartes.network>
2022-08-18 17:01:14 +0700

    add panels

--- src/static.app.tsx
+++ src/static.app.tsx
@@ -1,5 +1,6 @@
-import panel from 'static/images/panel.jpeg'
+import panel1 from 'static/images/panel1.jpeg'
+import panel2 from 'static/images/panel1.png'
 
-export const panels = [panel]
+export const panels = [panel1, panel2]
 export { default as logo } from 'static/images/logo.png'
 export { default as readme } from 'static/docs/README.md'
