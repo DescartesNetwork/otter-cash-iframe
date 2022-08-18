4f47eb95a564a364ccac9117b13fc03eec244484
tuphan-dn <tuphan@descartes.network>
2022-08-18 14:22:25 +0700

    bg

--- src/view/index.tsx
+++ src/view/index.tsx
@@ -1,3 +1,6 @@
+import { useEffect } from 'react'
+import { useUI } from '@sentre/senhub'
+
 import EmbededView from '@sentre/embeded-view'
 
 import configs from 'configs'
@@ -5,8 +8,15 @@ import configs from 'configs'
 const {
   manifest: { appId },
 } = configs
+const BG_IMG = 'linear-gradient(150deg, #ffcd9e, #e8ffb8)'
 
 const View = () => {
+  const { setBackground } = useUI()
+
+  useEffect(() => {
+    setBackground({ light: BG_IMG, dark: BG_IMG })
+  }, [setBackground])
+
   return (
     <EmbededView
       appId={appId}
